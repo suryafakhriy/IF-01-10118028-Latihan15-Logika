10bf34581b0f23012a18f620e946d97f1f69a5d5
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:51:22 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan15-Logika/src/pkgif/pkg01/pkg10118028/latihan15/logika/IF0110118028Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan15.logika;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Logika
+ */
+public class IF0110118028Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a= " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
